3fa66ac733f8d3a1a333b5d4a7d228a721917a5f
Perpetual Akinyi <you@example.com>
2024-10-07 12:36:09 +0300

    changes in dom

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code her
+const main = document.getElementById('main');
+main.remove();
+const newHeader  = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Perpetual is the champion';
+
